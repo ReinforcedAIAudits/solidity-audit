9fccf0633978ebe40df29a3ba2cb06cd9b3e6b6f
PraetorP <praetorian281@gmail.com>
2024-10-09 20:25:28 +0700

    fix: Docker build error

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ FROM python:3.11-slim
 ARG MNEMONIC_COLDKEY
 ARG MNEMONIC_HOTKEY
 ARG WALLET_NAME
-
+RUN apt-get update && apt-get install -y git
 WORKDIR /app
 
 
 COPY . /app
 
 RUN pip install -e .
+RUN pip install bittensor-cli
 
 RUN scripts/restore_or_create_wallets.sh MNEMONIC_COLDKEY MNEMONIC_HOTKEY WALLET_NAME
--- scripts/restore_or_create_wallets.sh
+++ scripts/restore_or_create_wallets.sh
@@ -4,9 +4,9 @@ MNEMONIC_COLDKEY=$1
 MNEMONIC_HOTKEY=$2
 NAME=${3:-foo}
 
-if [[ -z "$MNEMONIC_COLDKEY" && -z "$MNEMONIC_HOTKEY" ]]; then
-    btcli w create --name "$NAME" --no-use-password --quiet --wallet.path /root/.bittensor/wallets
-    btcli w create --name "$NAME" --no-use-password --quiet --wallet.path /root/.bittensor/wallets
+if [[ -z "$MNEMONIC_COLDKEY" -a -z "$MNEMONIC_HOTKEY" ]]; then
+    btcli w create --name "$NAME" --no-use-password --quiet --wallet.path ~/.bittensor/wallets
+    btcli w create --name "$NAME" --no-use-password --quiet --wallet.path ~/.bittensor/wallets
 else
     btcli w regen_coldkey --mnemonic "$MNEMONIC_COLDKEY" --name "$NAME" --no-use-password --quiet --wallet.path /root/.bittensor/wallets
     btcli w regen_hotkey --mnemonic "$MNEMONIC_HOTKEY" --name "$NAME" --no-use-password --quiet --wallet.path /root/.bittensor/wallets
