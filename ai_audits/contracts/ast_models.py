c4a8a553428a37ff292320aed5b80e3152397798
avskvortsov <alexskvr03@gmail.com>
2024-12-12 17:40:20 +0000

    upd: updates for structs and source logic

--- ai_audits/contracts/ast_models.py
+++ ai_audits/contracts/ast_models.py
@@ -1,139 +1,203 @@
+import enum
 from typing import Dict, List, Optional, Union
 from pydantic import BaseModel, Field
 
-class TypeDescriptions(BaseModel):
-    type_identifier: str = Field(alias="typeIdentifier")
-    type_string: str = Field(alias="typeString")
 
-class ElementaryTypeName(BaseModel):
+class NodeType(enum.Enum):
+    SOURCE_UNIT = "SourceUnit"
+    BLOCK = "Block"
+    PRAGMA_DIRECTIVE = "PragmaDirective"
+    CONTRACT_DEFINITION = "ContractDefinition"
+    FUNCTION_DEFINITION = "FunctionDefinition"
+    VARIABLE_DECLARATION = "VariableDeclaration"
+    VARIABLE_DECLARATION_STATEMENT = "VariableDeclarationStatement"
+    FUNCTION_CALL = "FunctionCall"
+    PARAMETER_LIST = "ParameterList"
+    EVENT_DEFINITION = "EventDefinition"
+    EMIT_STATEMENT = "EmitStatement"
+    ASSIGNMENT = "Assignment"
+    BINARY_OPERATION = "BinaryOperation"
+    UNARY_OPERATION = "UnaryOperation"
+    LITERAL = "Literal"
+    IDENTIFIER = "Identifier"
+    IDENTIFIER_PATH = "IdentifierPath"
+    MEMBER_ACCESS = "MemberAccess"
+    INDEX_ACCESS = "IndexAccess"
+    TUPLE_EXPRESSION = "TupleExpression"
+    EXPRESSION_STATEMENT = "ExpressionStatement"
+    RETURN = "Return"
+    ELEMENTARY_TYPE_NAME = "ElementaryTypeName"
+    USER_DEFINED_TYPE_NAME = "UserDefinedTypeName"
+    STRUCT_DEFINITION = "StructDefinition"
+    MAPPING = "Mapping"
+
+
+class NodeBase(BaseModel):
     id: int
-    name: str
-    node_type: str = Field(alias="nodeType")
     src: str
+    node_type: NodeType = Field(alias="nodeType")
+
+
+class TypeDescriptions(BaseModel):
+    type_identifier: Optional[str] = Field(alias="typeIdentifier")
+    type_string: Optional[str] = Field(alias="typeString")
+
+
+class ElementaryTypeName(NodeBase):
+    name: str
     type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
     state_mutability: Optional[str] = Field(default=None, alias="stateMutability")
 
-class Mapping(BaseModel):
-    id: int
+
+class PathNode(NodeBase):
+    name: str
+    name_locations: List[str] = Field(alias="nameLocations")
+    node_type: str = Field(alias="nodeType")
+    referenced_declaration: int = Field(alias="referencedDeclaration")
+
+
+class UserDefinedTypeName(NodeBase):
+    path_node: PathNode = Field(alias="pathNode")
+    referenced_declaration: int = Field(alias="referencedDeclaration")
+    type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
+
+
+class Mapping(NodeBase):
     key_name: str = Field(alias="keyName")
     key_name_location: str = Field(alias="keyNameLocation")
     key_type: ElementaryTypeName = Field(alias="keyType")
-    node_type: str = Field(alias="nodeType")
-    src: str
     type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
     value_name: str = Field(alias="valueName")
     value_name_location: str = Field(alias="valueNameLocation")
-    value_type: ElementaryTypeName = Field(alias="valueType")
+    value_type: Union[UserDefinedTypeName, ElementaryTypeName] = Field(
+        alias="valueType"
+    )
 
-class VariableDeclaration(BaseModel):
+
+class VariableDeclaration(NodeBase):
     constant: bool
-    function_selector: Optional[str] = Field(alias="functionSelector")
-    id: int
+    function_selector: Optional[str] = Field(default=None, alias="functionSelector")
     mutability: str
     name: str
-    name_location: str = Field(alias="nameLocation")
-    node_type: str = Field(alias="nodeType")
+    name_location: Optional[str] = Field(alias="nameLocation")
     scope: int
-    src: str
     state_variable: bool = Field(alias="stateVariable")
     storage_location: str = Field(alias="storageLocation")
     type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
     type_name: Union[Mapping, ElementaryTypeName] = Field(alias="typeName")
     visibility: str
 
-class ParameterList(BaseModel):
-    id: int
-    node_type: str = Field(alias="nodeType")
-    parameters: List[dict]
-    src: str
 
-class Identifier(BaseModel):
-    id: int
+class ParameterList(NodeBase):
+    parameters: List[VariableDeclaration]
+
+
+class Identifier(NodeBase):
     name: str
-    node_type: str = Field(alias="nodeType")
     overloaded_declarations: List[int] = Field(alias="overloadedDeclarations")
     referenced_declaration: int = Field(alias="referencedDeclaration")
-    src: str
     type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
 
-class MemberAccess(BaseModel):
+
+class Return(NodeBase):
+    function_return_parameters: int = Field(alias="functionReturnParameters")
     expression: Identifier
-    id: int
+
+
+class MemberAccess(NodeBase):
+    expression: Identifier
     is_constant: bool = Field(alias="isConstant")
     is_lvalue: bool = Field(alias="isLValue")
     is_pure: bool = Field(alias="isPure")
     lvalue_requested: bool = Field(alias="lValueRequested")
     member_location: str = Field(alias="memberLocation")
     member_name: str = Field(alias="memberName")
-    node_type: str = Field(alias="nodeType")
-    src: str
+    sub_expression: Optional[Identifier] = Field(default=None, alias="subExpression")
     type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
 
-class Assignment(BaseModel):
-    id: int
+
+class Assignment(NodeBase):
     is_constant: bool = Field(alias="isConstant")
     is_lvalue: bool = Field(alias="isLValue")
     is_pure: bool = Field(alias="isPure")
     lvalue_requested: bool = Field(alias="lValueRequested")
-    left_hand_side: Union[Identifier, Dict] = Field(alias="leftHandSide")
-    node_type: str = Field(alias="nodeType")
+    left_hand_side: Optional[Union[Identifier, Dict]] = Field(default=None, alias="leftHandSide")
     operator: str
-    right_hand_side: Union[MemberAccess, Dict] = Field(alias="rightHandSide")
-    src: str
+    prefix: Optional[bool] = Field(default=None)
+    right_hand_side: Optional[Union[MemberAccess, Identifier, Dict]] = Field(default=None,
+        alias="rightHandSide"
+    )
+    sub_expression: Optional[Identifier] = Field(default=None, alias="subExpression")
     type_descriptions: TypeDescriptions = Field(alias="typeDescriptions")
 
-class ExpressionStatement(BaseModel):
+
+class ExpressionStatement(NodeBase):
     expression: Assignment
-    id: int
-    node_type: str = Field(alias="nodeType")
-    src: str
 
-class Block(BaseModel):
-    id: int
-    node_type: str = Field(alias="nodeType")
-    src: str
-    statements: List[ExpressionStatement]
 
-class FunctionDefinition(BaseModel):
+class Block(NodeBase):
+    statements: List[Union[ExpressionStatement, Return]]
+
+
+class FunctionDefinition(NodeBase):
     body: Block
     function_selector: Optional[str] = Field(default=None, alias="functionSelector")
-    id: int
-    implemented: bool
+    implemented: bool = True
     kind: str
-    modifiers: List
-    name: str
+    modifiers: List = Field(default_factory=list)
     name_location: str = Field(alias="nameLocation")
-    node_type: str = Field(alias="nodeType")
     parameters: ParameterList
     return_parameters: ParameterList = Field(alias="returnParameters")
     scope: int
-    src: str
+    name: str
     state_mutability: str = Field(alias="stateMutability")
-    virtual: bool
+    virtual: bool = False
     visibility: str
 
-class ContractDefinition(BaseModel):
+
+class StructDefinition(NodeBase):
+    canonical_name: str = Field(alias="canonicalName")
+    members: List[VariableDeclaration | FunctionDefinition]
+    name_location: str = Field(alias="nameLocation")
+    scope: int
+    name: str
+    visibility: str
+
+
+class PragmaDirective(NodeBase):
+    literals: List[str]
+
+
+class EventDefinition(NodeBase):
+    anonymous: bool
+    event_selector: str = Field(alias="eventSelector")
+    name_location: str = Field(alias="nameLocation")
+    parameters: ParameterList
+    name: str
+
+
+class ContractDefinition(NodeBase):
     abstract: bool
     base_contracts: List = Field(alias="baseContracts")
     canonical_name: str = Field(alias="canonicalName")
     contract_dependencies: List = Field(alias="contractDependencies")
     contract_kind: str = Field(alias="contractKind")
     fully_implemented: bool = Field(alias="fullyImplemented")
-    id: int
     linearized_base_contracts: List[int] = Field(alias="linearizedBaseContracts")
-    name: str
     name_location: str = Field(alias="nameLocation")
-    node_type: str = Field(alias="nodeType")
-    nodes: List[Union[VariableDeclaration, FunctionDefinition]]
+    nodes: List[
+        Union[
+            VariableDeclaration, StructDefinition, FunctionDefinition, EventDefinition
+        ]
+    ]
     scope: int
-    src: str
+    name: str
     used_errors: List = Field(alias="usedErrors")
     used_events: List = Field(alias="usedEvents")
 
-class SourceUnit(BaseModel):
+
+class SourceUnit(NodeBase):
+    license: Optional[str] = Field(default=None)
     absolute_path: str = Field(alias="absolutePath")
     exported_symbols: Dict[str, List[int]] = Field(alias="exportedSymbols")
-    id: int
-    node_type: str = Field(alias="nodeType")
-    nodes: List[ContractDefinition]
-    src: str
+    nodes: List[Union[ContractDefinition, PragmaDirective]]
