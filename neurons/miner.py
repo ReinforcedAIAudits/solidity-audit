9fb2a3d86343e4e2b2da92862d980efb94b6165f
avskvortsov <alexskvr03@gmail.com>
2025-02-09 19:02:51 +0000

    upd: updated logging for last bt version

--- neurons/miner.py
+++ neurons/miner.py
@@ -20,6 +20,7 @@ import os
 import time
 import typing
 import bittensor as bt
+from bittensor.utils.btlogging import logging
 from dotenv import load_dotenv
 
 from ai_audits.subnet_utils import create_session
@@ -55,13 +56,13 @@ class Miner(ReinforcedMinerNeuron):
                         set(self._dendrite_whitelist) | set(keys_list)
                     )
                 else:
-                    bt.logging.error(f"key list has invalid format: {keys_list}")
+                    logging.error(f"key list has invalid format: {keys_list}")
             else:
-                bt.logging.info(
+                logging.info(
                     f"Something went wrong with the key service. Status code: {keys_response.status_code}"
                 )
         except Exception as e:
-            bt.logging.error(f"An error occurred while connection to key service: {e}")
+            logging.error(f"An error occurred while connection to key service: {e}")
 
     @classmethod
     def do_audit_code(cls, contract_code: str) -> list[VulnerabilityReport]:
@@ -72,11 +73,11 @@ class Miner(ReinforcedMinerNeuron):
         )
         # TODO: Remove the error and allow sending a synapse with an empty response + log this event
         if result.status_code != 200:
-            bt.logging.info(f"Not successful AI response. Description: {result.text}")
+            logging.info(f"Not successful AI response. Description: {result.text}")
             raise ValueError("Contract audit is not successful!")
 
         json = result.json()
-        bt.logging.info(f"Response from model server: {json}")
+        logging.info(f"Response from model server: {json}")
         vulnerabilities = [
             VulnerabilityReport(
                 **vuln,
@@ -89,7 +90,7 @@ class Miner(ReinforcedMinerNeuron):
         """
         Processes the incoming synapse by performing a predefined operation on the input data.
         """
-        bt.logging.info(f"Received synapse from validator {synapse}")
+        logging.info(f"Received synapse from validator {synapse}")
 
         vulnerabilities = self.do_audit_code(synapse.contract_code)
 
@@ -101,7 +102,7 @@ class Miner(ReinforcedMinerNeuron):
         Determines whether an incoming request should be blacklisted and thus ignored.
         """
         if synapse.dendrite is None or synapse.dendrite.hotkey is None:
-            bt.logging.warning("Received a request without a dendrite or hotkey.")
+            logging.warning("Received a request without a dendrite or hotkey.")
             return True, "Missing dendrite or hotkey"
 
         if synapse.dendrite.hotkey in self._dendrite_whitelist:
@@ -113,7 +114,7 @@ class Miner(ReinforcedMinerNeuron):
             and synapse.dendrite.hotkey not in self.metagraph.hotkeys
         ):
             # Ignore requests from un-registered entities.
-            bt.logging.trace(
+            logging.trace(
                 f"Blacklisting un-registered hotkey {synapse.dendrite.hotkey}"
             )
             return True, "Unrecognized hotkey"
@@ -138,12 +139,12 @@ class Miner(ReinforcedMinerNeuron):
         if self.config.blacklist.force_validator_permit:
             # If the config is set to force validator permit, then we should only allow requests from validators.
             if not self.metagraph.validator_permit[uid]:
-                bt.logging.warning(
+                logging.warning(
                     f"Blacklisting a request from non-validator hotkey {synapse.dendrite.hotkey}"
                 )
                 return True, "Non-validator hotkey"
 
-        bt.logging.trace(
+        logging.trace(
             f"Not Blacklisting recognized hotkey {synapse.dendrite.hotkey}"
         )
         return False, "Hotkey recognized!"
@@ -154,7 +155,7 @@ class Miner(ReinforcedMinerNeuron):
         requests are processed before others.
         """
         if synapse.dendrite is None or synapse.dendrite.hotkey is None:
-            bt.logging.warning("Received a request without a dendrite or hotkey.")
+            logging.warning("Received a request without a dendrite or hotkey.")
             return 0.0
 
         if synapse.dendrite.hotkey in self._dendrite_whitelist:
@@ -167,7 +168,7 @@ class Miner(ReinforcedMinerNeuron):
         priority = float(
             self.metagraph.S[caller_uid]
         )  # Return the stake as the priority.
-        bt.logging.trace(
+        logging.trace(
             f"Prioritizing {synapse.dendrite.hotkey} with value: {priority}"
         )
         return priority
@@ -181,5 +182,5 @@ if __name__ == "__main__":
     load_dotenv()
     with Miner() as miner:
         while True:
-            bt.logging.info(f"Miner running... {time.time()}")
+            logging.info(f"Miner running... {time.time()}")
             time.sleep(1200)
