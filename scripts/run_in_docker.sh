7405c9d73cdf9ee4fd8b1903b4c15472b709c013
avskvortsov <alexskvr03@gmail.com>
2025-02-17 09:42:21 +0000

    upd: added SKIP_HEALTHCHECK

--- scripts/run_in_docker.sh
+++ scripts/run_in_docker.sh
@@ -7,17 +7,21 @@ check_endpoint() {
     echo "$response"
 }
 
-while true; do
-    status_code=$(check_endpoint)
+if [ -z "${SKIP_HEALTHCHECK}" ]; then
+    while true; do
+        status_code=$(check_endpoint)
 
-    if [ "$status_code" -eq 200 ]; then
-        break
-    else
-        echo "Model server is not running yet."
-        echo "Retrying in 5 seconds..."
-        sleep 5
-    fi
-done
+        if [ "$status_code" -eq 200 ]; then
+            break
+        else
+            echo "Model server is not running yet."
+            echo "Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+else
+    echo "Healthcheck skipped due to SKIP_HEALTHCHECK variable"
+fi
 
 if [ "$WORKER_TYPE" == "miner" ]; then 
     python neurons/miner.py --netuid "${NETWORK_UID}" --wallet.name "${WALLET_NAME}" --wallet.hotkey "${WALLET_HOTKEY}" --subtensor.network "${NETWORK_TYPE}" --subtensor.chain_endpoint "${CHAIN_ENDPOINT}" --axon.external_ip "${EXTERNAL_IP}" --logging.debug
